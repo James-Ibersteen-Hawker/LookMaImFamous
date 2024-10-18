71a59c2601bc690233bb3d379310cded3d61d8d4
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2024-10-18 11:51:54 -0400

    Finishing up for today

--- script.js
+++ script.js
@@ -14,13 +14,22 @@ function bigPic() {
 let opened = false;
 function open_nav() {
   let menu = document.getElementsByClassName("mega-menu")[0];
+  let image = document.getElementsByClassName("jumbo-image")[0];
   if (opened == false) {
     menu.classList.remove("up");
     menu.classList.add("down");
+    image.classList.remove("downpic");
+    image.classList.add("uppic");
     opened = true;
   } else if (opened == true) {
     menu.classList.remove("down");
     menu.classList.add("up");
+    image.classList.remove("uppic");
+    image.classList.add("downpic");
     opened = false;
   }
+}
+
+function linker(link) {
+  window.location = link;
 }
